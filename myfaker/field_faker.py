7403bc70c8ae30aab1e3de69f80fa69c7904cdad
Lizhi <lizhi2754@idss-cn.com>
2024-02-06 14:25:11 +0800

    code mktablename and mkfield

---
+++ myfaker/field_faker.py
@@ -0,0 +1,54 @@
+
+from faker import Faker
+from mimesis.schema import Field
+from mimesis.locales import Locale
+from mimesis.enums import TimestampFormat
+from mimesis.enums import Gender
+
+
+fake = Faker(locale='zh_CN')
+field = Field(locale=Locale.ZH)
+
+# 字段名称和对应模拟值的字典，动态匿名函数
+fields_template = {
+    "pk": field("increment"),
+    "uid": field("uuid"),
+    "name": field("person.name"),
+    "username": field("person.username"),
+    "memo": field("text.word"),
+    "text": fake.sentence(nb_words=6, variable_nb_words=True, ext_word_list=None) ,
+    "version": field("version"),
+    "timestamp": field("timestamp", fmt=TimestampFormat.RFC_3339),
+    "email": field("person.email", domains=["idss-cn.com"]),
+    "creator": field("full_name", gender=Gender.FEMALE),
+    "apiKeys": field("uuid"),
+}
+
+# 从上述字典名称字典中，随机抽取一些字段组成新的字段名称和模拟值的新字典，而且字典名称后要加随机数来使字段名不一致
+import copy
+import random
+def mkfields():
+    fields = {}
+    tmp_fields = copy.deepcopy(fields_template)
+
+    # 设置输出字段数量，数量是随机取5到默认字典的一个随机数
+    random_count = random.randint(5, len(fields_template))
+    for i in range(random_count):
+        item = tmp_fields.popitem()
+        # print(item[0], item[1])
+        k = item[0] + '_' + str(random.randint(100,10000))
+        fields[k] = item[1]
+
+    return fields
+
+    # print(erase_count, len(fields_template)-erase_count)
+    # for i in range(len(fields_template)-erase_count):
+    #     fields.popitem()
+    # return fields
+
+
+if __name__ == '__main__':
+    print('test make fields definition from template dict')
+    for i in range(5):
+        fields = mkfields()
+        print(len(fields), fields)
--- myfaker/tablename_faker.py
+++ myfaker/tablename_faker.py
@@ -1,2 +1,30 @@
+from mimesis import Field, Locale
 
 
+# 使用用户名来产生一个单词，以后应该考虑使用一个名称字典来随机产生
+# mktablename() 产生一个单词
+def mktablename():
+    field = Field(Locale.EN)
+    tablename = field("username", mask="U_d", key=str.lower, drange=(100, 10000))
+    return tablename
+
+# mktablenames(nums) 一次产生nums个单词，同时需要判断是否重复，故使用一个临时字典判断是否重复
+def mktablenames(nums):
+    tablenames = {}
+    while len(tablenames) < nums:
+        tablename = mktablename()
+        if tablenames.get(tablename) is None:
+            tablenames[tablename] = 1
+    return list(tablenames.keys())
+
+
+if __name__ == '__main__':
+    print('test make tablename from mimesis')
+    print('random make tablename for 5 times')
+    for i in range(5):
+        tablename = mktablename()
+        print(len(tablename), tablename)
+
+    print('random make tablenames in one time')
+    tablenames = mktablenames(9)
+    print(len(tablenames), tablenames)
